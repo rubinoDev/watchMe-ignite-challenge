cc643f53bc8d28aec86d03ba1c6c4427649bf63b
Victor Rubino <rubinodev@gmail.com>
2022-07-07 16:33:51 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+import './styles/global.scss';
+import './styles/sidebar.scss';
+import './styles/content.scss';
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar handleClickButton={handleClickButton} selectedGenreId={selectedGenreId}/>
+      <Content selectedGenreId={selectedGenreId}/>  
+
+      
+    </div>
+  )
+}
